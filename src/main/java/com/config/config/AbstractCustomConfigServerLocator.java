66fc2e85f80fcbd3f7b75255ba8023d81cb84b78
jzmzpn <287672545@qq.com>
2018-09-23 22:34:13 +0800

    调整一下

--- src/main/java/com/config/config/AbstractCustomConfigServerLocator.java
+++ src/main/java/com/config/config/AbstractCustomConfigServerLocator.java
@@ -12,23 +12,39 @@ public abstract class AbstractCustomConfigServerLocator implements PropertySourc
     @Override
     public PropertySource<?> locate(Environment environment) {
         this.environment = environment;
-        Map<String, String> properties = getMap(environment);
+        Map<String, String> properties = getMap();
 
         CutomPropertySource cutomPropertySource = new CutomPropertySource("cutomPropertySource", properties);
         return cutomPropertySource;
     }
 
+    public Environment getEnvironment() {
+        return this.environment;
+    }
+
+    /*
+    * 获取定义中的application名称
+    * */
     public String getApplicationName() {
         return environment.getProperty("spring.application.name");
     }
 
+    /*
+     * 获取定义中的profile
+     * */
     public String getProfile() {
         return environment.getProperty("spring.cloud.config.profile") == null ? "default" : environment.getProperty("spring.cloud.config.profile");
     }
 
+    /*
+     * 获取定义中的label
+     * */
     public String getLabel() {
         return environment.getProperty("spring.cloud.config.label") == null ? "master" : environment.getProperty("spring.cloud.config.label");
     }
 
-    public abstract Map<String, String> getMap(Environment environment);
+    /*
+    * 自定义返回Map类型的配置信息
+    * */
+    public abstract Map<String, String> getMap();
 }
